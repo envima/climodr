2cbe8cd2a251a4d1bba2fb77cd1a0c855a1a70e4
Alexander Klug <56920208+Kluga@users.noreply.github.com>
2022-10-31 13:52:31 +0100

    Update

--- testing/Testing.R
+++ testing/Testing.R
@@ -1,18 +1,18 @@
 devtools::load_all()
 
 ### Initiate Climodr ###
-climodr::hubs("C:/Users/Alexander/Documents/Universität/HiWi/Test/data/Input/",
-     "C:/Users/Alexander/Documents/Universität/HiWi/Test/data/Output/")
+climodr::hubs("C:/Users/Alexander/Documents/Uni/HiWi/Test/data/Input/",
+     "C:/Users/Alexander/Documents/Uni/HiWi/Test/data/Output/")
 
-climodr::envi.create("C:/Users/Alexander/Documents/Universität/HiWi/Test/data/")
+climodr::envi.create("C:/Users/Alexander/Documents/Uni/HiWi/Test/data/")
 
 ### Pre-Processing ###
 
 # Cropping
 
-climodr::crop.all(method = "Input", safe_output = TRUE)
+x <- climodr::crop.all(method = "Input", safe_output = TRUE)
 
 x <- terra::rast(paste0(Output, "tiff_stack.tif"))
 x
-terra::plot(x)
+terra::plot(x[[1:4]])
 
